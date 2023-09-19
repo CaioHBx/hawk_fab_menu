0b2ef18208e49f5b24a4f670ec01899e0c93380c
Hans Mateus Boron <hans.boron@bsoft.com.br>
2023-09-19 10:55:32 -0300

    Revert "feat: tooltip sem permissão quando desabilitado"
    This reverts commit d0fa4e52479c64aa9fbfe9de69e33f9e3f27242a.

--- lib/hawk_fab_menu.dart
+++ lib/hawk_fab_menu.dart
@@ -241,49 +241,46 @@ class _MenuItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: item.enabled == false ? 'Sem permissão' : null,
-      child: InkWell(
-        onTap: item.enabled ? onTap : null,
-        child: Column(
-          children: [
-            Row(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 3,
-                  ),
-                  decoration: BoxDecoration(
-                    color: item.enabled ? (item.labelBackgroundColor ?? Colors.white) : Theme.of(context).disabledColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    item.label,
-                    style: TextStyle(color: item.labelColor ?? Colors.black87),
-                  ),
+    return InkWell(
+      onTap: item.enabled ? onTap : null,
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 3,
                 ),
-                const SizedBox(
-                  width: 5,
+                decoration: BoxDecoration(
+                  color: item.enabled ? (item.labelBackgroundColor ?? Colors.white) : Theme.of(context).disabledColor,
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: FloatingActionButton(
-                    elevation: 0,
-                    onPressed: item.enabled ? onTap : null,
-                    heroTag: item.heroTag ?? '_MenuItemWidget_$hashCode',
-                    child: item.icon,
-                    backgroundColor: item.enabled ? (item.color ?? Theme.of(context).primaryColor) : Theme.of(context).disabledColor,
-                  ),
+                child: Text(
+                  item.label,
+                  style: TextStyle(color: item.labelColor ?? Colors.black87),
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-          ],
-        ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              SizedBox(
+                height: 50,
+                width: 50,
+                child: FloatingActionButton(
+                  elevation: 0,
+                  onPressed: item.enabled ? onTap : null,
+                  heroTag: item.heroTag ?? '_MenuItemWidget_$hashCode',
+                  child: item.icon,
+                  backgroundColor: item.enabled ? (item.color ?? Theme.of(context).primaryColor) : Theme.of(context).disabledColor,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+        ],
       ),
     );
   }
